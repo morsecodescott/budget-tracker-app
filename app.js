e0a2f21349b599b10ff6c6605699a7e6ac2048d8
Scott Morse <scott.m.morse@gmail.com>
2024-03-21 23:34:25 +0000

    fixed mongo connection

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ require('dotenv').config();
 const app = express();
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
