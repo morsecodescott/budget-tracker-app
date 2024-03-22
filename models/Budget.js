0244607579771df24618cde9141bada484ad0473
Scott Morse <scott.m.morse@gmail.com>
2024-03-22 19:28:30 +0000

    added registration page

--- models/Budget.js
+++ models/Budget.js
@@ -10,3 +10,4 @@ const budgetSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Budget', budgetSchema);
+
--- routes/auth.js
+++ routes/auth.js
@@ -13,19 +13,31 @@ router.post(
   })
 );
 
+
+// Registration route - GET
+router.get('/register', (req, res) => {
+  res.render('register'); // Render registration form
+});
+
+
+
+
+// Registration route - POST
 router.post('/register', (req, res) => {
   const { username, password } = req.body;
+  // Check if user already exists
   User.findOne({ username: username }).then((user) => {
-    if (user) {
-      req.flash('error_msg', 'Username already exists');
-      res.redirect('/register');
-    } else {
-      const newUser = new User({ username, password });
-      newUser.save().then(() => {
-        req.flash('success_msg', 'You are now registered and can log in');
-        res.redirect('/login');
-      });
-    }
+      if (user) {
+          req.flash('error_msg', 'Username already exists');
+          res.redirect('/auth/register');
+      } else {
+          // Create new user
+          const newUser = new User({ username, password });
+          newUser.save().then(() => {
+              req.flash('success_msg', 'You are now registered and can log in');
+              res.redirect('/auth/login');
+          });
+      }
   });
 });
 
