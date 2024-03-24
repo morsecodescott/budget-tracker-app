47e1589a286a7da4b977e6689f81ea3056cb0d2d
Scott Morse <scott.m.morse@gmail.com>
2024-03-24 01:02:17 -0300

    renamed html files to ejs file

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-// routes/index.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('Welcome to the budget tracker');
-});
-
-module.exports = router;
