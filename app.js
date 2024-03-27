c1896459dbbb4a26d55de67db6d7721b2c5becb9
Scott Morse <scott.m.morse@gmail.com>
2024-03-26 22:16:47 -0300

    fixed flash messages

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use((req, res, next) => {
   res.locals.appSlogan = appSlogan;
   res.locals.currentUser = req.user; // Set currentUser from req.user
   res.locals.error_msg = req.flash('error_msg');
-  res.locals.error_msg = req.flash('success_msg');
+  res.locals.success_msg = req.flash('success_msg');
   next();
 });
 
