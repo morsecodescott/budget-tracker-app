4a222eb6dab8275210e59c63940b9428a9edfb9d
Scott <scott.m.morse@gmail.com>
2024-04-09 22:13:41 -0300

    fixed error on sighup but using async/await

--- app.js
+++ app.js
@@ -46,12 +46,17 @@ const db = mongoose.connection;
 db.on('connected', () => console.log('Connected to MongoDB.'));
 db.on('error', (err) => console.error(`MongoDB connection error: ${err.message}`));
 db.on('disconnected', () => console.log('Disconnected from MongoDB.'));
-process.on('SIGINT', () => {
-  db.close(() => {
+process.on('SIGINT', async () => {
+  try {
+    await db.close();
     console.log('MongoDB connection closed due to app termination');
     process.exit(0);
-  });
+  } catch (error) {
+    console.error('Failed to close MongoDB connection:', error);
+    process.exit(1);
+  }
 });
+
 
 // App settings and middleware
 app.set('view engine', 'ejs');
