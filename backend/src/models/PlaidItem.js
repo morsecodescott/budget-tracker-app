54967f6acc7815d6903752a7c0809c0913503dd3
Scott <scott.m.morse@gmail.com>
2024-05-07 20:44:20 -0300

    added institution id

--- backend/src/models/PlaidItem.js
+++ backend/src/models/PlaidItem.js
@@ -4,6 +4,7 @@ const plaidItemSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     accessToken: { type: String, required: true },
     itemId: { type: String, required: true },
+    institutionId: { type: String, required: true },
     institutionName: { type: String, required: true },
     accounts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'PlaidAccount' }]
 });
